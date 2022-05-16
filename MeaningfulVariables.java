90c1daf63ab019bffdab01a65116576120b6329b
ANKIT KUMAR <68455254+ankitjha-webdev@users.noreply.github.com>
2022-05-16 14:56:46 +0530

    MeaningfulVariables.java

---
+++ MeaningfulVariables.java
@@ -0,0 +1,10 @@
+class MeaningfulVariables{
+    public static void main(String[] args){
+        int age=20;
+        String name="John";
+        double height=1.8;
+        boolean isMale=true;
+        System.out.println("My name is "+name+" and I am "+age+" years old. As well as "+height+" m tall. I am "+isMale+".");
+
+    }
+}
