45deb7caa666c8633cde27290682d463b56320f9
ANKIT KUMAR <68455254+ankitjha-webdev@users.noreply.github.com>
2022-05-16 14:57:16 +0530

    RadiusProgram.java

---
+++ RadiusProgram.java
@@ -0,0 +1,7 @@
+public class RadiusProgram {
+    public static void main(String[] args) {
+        double radius = 1.8;
+        double area = radius * radius * Math.PI;
+        System.out.println("The area of the circle is " + area);
+    }
+}
