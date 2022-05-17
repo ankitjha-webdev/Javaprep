fb2bf7dff55ed44dc38e6874a818e8a6625703ca
ANKIT KUMAR <68455254+ankitjha-webdev@users.noreply.github.com>
2022-05-17 20:46:55 +0530

    OppositeTable.java

---
+++ OppositeTable.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class OppositeTable {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=10; i>=1; i--){
+            System.out.println(n+" x "+i+" = "+n*i);  
+        }
+    }
+}
